3a2ecc729d1ec888f619aa97b743175011221809
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-07-30 14:04:30 -0600

    Styled the application

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   if (props.phonetic) {
     return (
       <div className="Phonetic">
-        <h2>{props.phonetic}</h2>
+        <span className="text">{props.phonetic}</span>
       </div>
     );
   } else {
